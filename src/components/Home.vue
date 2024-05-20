23852a68ac54e31388986873f6dcc8dfc40a233f
liangjin0228 <liangjin0228@gmail.com>
2024-05-20 17:48:59 +0800

    Hello

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,50 +1,68 @@
 <template>
-    <v-card-title><v-avatar size="150px">
-            <v-img alt="mulin"
-                src="https://lh3.googleusercontent.com/jst1v7bWdoGDXguKOFekfbrGY5ZHSgL6Dzek5snBS8AqSnyTLa7sYCNgy1Si_FWupfXamqJsUPVhh2lxX9al1IQ=w1280"></v-img>
-        </v-avatar></v-card-title>
-    <v-card-title class="text-light-green-darken-4">Maureen Lin</v-card-title>
-    <v-card-text>
-        <v-icon icon="mdi-email-outline" size="x-small"></v-icon>
-        E-mail: <br>
-        mumulin1013[at]gmail.com <br>
-        <v-icon icon="mdi-briefcase-outline" size="x-small"></v-icon>
-        Office: <br>
-        Tjing Ling Industrial Building 222, National Taiwan University
-    </v-card-text>
-    <v-divider></v-divider>
-    <v-card-title>Research Interest</v-card-title>
-    <v-card-text>
-        Experienced Software Engineer with a demonstrated history of working.<br>
-        Skilled in Full-stack development, Database Management, Data Mining, Analytics & Visualization.
-    </v-card-text>
-    <v-divider></v-divider>
-    <v-card-title>Education</v-card-title>
-    <v-card-text>
-        <v-list-item v-for=" edu in edus">
-            <a>{{ edu.department }}</a>
-            <v-list-item-subtitle>{{ edu.school }}</v-list-item-subtitle>
-            <v-list-item-subtitle>{{ edu.time }}</v-list-item-subtitle>
-        </v-list-item>
-    </v-card-text>
-    <v-divider></v-divider>
-    <v-card-title>Education</v-card-title>
-    <v-card-text>
-        <v-list-item v-for=" exp in exps">
-            <a>{{ exp.office }}</a>
-            <v-list-item-subtitle>{{ exp.company }}</v-list-item-subtitle>
-            <v-list-item-subtitle>{{ exp.time }}</v-list-item-subtitle>
-        </v-list-item>
-    </v-card-text>
+    <v-card class="ma-5">
+        <v-card-title>
+            <v-avatar size="150px">
+                <v-img alt="mulin"
+                    src="https://lh3.googleusercontent.com/jst1v7bWdoGDXguKOFekfbrGY5ZHSgL6Dzek5snBS8AqSnyTLa7sYCNgy1Si_FWupfXamqJsUPVhh2lxX9al1IQ=w1280"></v-img>
+            </v-avatar>
+            <v-container fluid class="pa-0 text-light-green-darken-4">Maureen Lin</v-container>
+            <v-container fluid class="px-0 text-subtitle-2">
+                <v-icon icon="mdi-email-outline" size="x-small"></v-icon>
+                E-mail: <br />
+                mumulin1013[at]gmail.com <br />
+                <v-icon icon="mdi-briefcase-outline" size="x-small"></v-icon>
+                Office: <br />
+                Tjing Ling Industrial Building 222, National Taiwan University
+            </v-container>
+        </v-card-title>
+
+        <v-card-text>
+            <v-divider></v-divider>
+            <v-container fluid class="px-0">
+                <span class="text-h6">Research Interest</span> <br>
+                <v-list-item>
+                    Experienced Software Engineer with a demonstrated history of working. <br>
+                    Skilled in Full-stack development, Database Management, Data Mining,
+                    Analytics & Visualization.
+                </v-list-item>
+            </v-container>
+
+            <v-divider></v-divider>
+            <v-container fluid class="px-0">
+                <span class="text-h6">Education</span> <br>
+                <v-list-item v-for="edu in edus">
+                    <span>{{ edu.department }}</span>
+                    <v-list-item-subtitle>
+                        {{ edu.school }} <br>
+                        {{ edu.time }}
+                    </v-list-item-subtitle>
+                </v-list-item>
+            </v-container>
+
+            <v-divider></v-divider>
+            <v-container fluid class="px-0">
+                <span class="text-h6">Experience</span> <br>
+                <v-list-item v-for="exp in exps">
+                    <span>{{ exp.office }}</span>
+                    <v-list-item-subtitle>
+                        {{ exp.company }} <br>
+                        {{ exp.time }}
+                    </v-list-item-subtitle>
+                </v-list-item>
+            </v-container>
+        </v-card-text>
+    </v-card>
 </template>
 
 <script>
-import { edus, exps } from "./Home.js"
+import { edus, exps } from "./Home.js";
 
 export default {
-    data: () => ({
-        edus: edus,
-        exps: exps
-    }),
-}
+    data() {
+        return {
+            edus: edus,
+            exps: exps
+        };
+    }
+};
 </script>
--- src/components/Useful.vue
+++ src/components/Useful.vue
@@ -1,21 +1,18 @@
 <template>
-    <v-main>
-      <v-empty-state
-        image="https://vuetifyjs.b-cdn.net/docs/images/components/v-empty-state/astro-dog.svg"
-        size="200"
-        text-width="250"
-      >
-        <template v-slot:media>
-          <v-img class="mb-8"></v-img>
+    <v-empty-state image="https://vuetifyjs.b-cdn.net/docs/images/components/v-empty-state/astro-dog.svg" size="200"
+        text-width="250">
+        <template #media>
+            <v-img class="mb-8"></v-img>
         </template>
 
-        <template v-slot:title>
-          <div class="text-h6 text-high-emphasis">Empty in drafts</div>
+        <template #title>
+            <div class="text-h6 text-high-emphasis">Empty in drafts</div>
         </template>
 
-        <template v-slot:text>
-          <div class="text-body-1">Save a draft message and it will show up here</div>
+        <template #text>
+            <div class="text-body-1">
+                Save a draft message and it will show up here
+            </div>
         </template>
-      </v-empty-state>
-    </v-main>
+    </v-empty-state>
 </template>
